49765f51ea91532db5badc3becc384c344402c58
Matthew Fortunka <mafortun@microsoft.com>
2021-04-30 09:12:35 +0100

    added arm demo

---
+++ deploy.ps1
@@ -0,0 +1,12 @@
+# $jsonRaw = Get-Content ".\storageParameters.json" | Out-String | ConvertFrom-Json 
+# $jsonParams = ($jsonRaw | ConvertTo-Json -Depth 30 -Compress).Replace('"', '\"') 
+
+
+# Write-Host $jsonParams
+$rg = "arm-test"
+
+az group create -n $rg --location "uksouth"
+
+New-AzResourceGroupDeployment -ResourceGroup $rg -TemplateFile ".\storage.json" -TemplateParameterFile ".\storageParameters.json"
+
+# az deployment group create -g $rg --template-file ".\storage.json" --parameters $jsonParams -n "dep-test" --confirm-with-what-if --what-if-result-format FullResourcePayloads
