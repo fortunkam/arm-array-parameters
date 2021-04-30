4d33e34b6f638e13fce18121d9787dcdf4fc8b6b
Matthew Fortunka <mafortun@microsoft.com>
2021-04-30 09:23:03 +0100

    added main template

--- deploy.ps1
+++ deploy.ps1
@@ -7,6 +7,6 @@ $rg = "arm-test"
 
 az group create -n $rg --location "uksouth"
 
-New-AzResourceGroupDeployment -ResourceGroup $rg -TemplateFile ".\storage.json" -TemplateParameterFile ".\storageParameters.json"
-
+#New-AzResourceGroupDeployment -ResourceGroup $rg -TemplateFile ".\storage.json" -TemplateParameterFile ".\storageParameters.json"
+New-AzResourceGroupDeployment -ResourceGroup $rg -TemplateFile ".\main.json"
 # az deployment group create -g $rg --template-file ".\storage.json" --parameters $jsonParams -n "dep-test" --confirm-with-what-if --what-if-result-format FullResourcePayloads
